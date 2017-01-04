277237e5ceef7b6c8bd1fc68fb6b57f691c1b255
Administrator <admin@example.com>
2017-01-04 13:25:24 +0530

    client listening url changed

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var socket = io('http://localhost:3000');
+    var socket = io('http://mychaty.herokuapp.com');
     var user = {};
     var username = '';
     var name = '';
